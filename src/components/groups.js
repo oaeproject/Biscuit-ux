13610f1a297a72fc399dfd98b8dc726839489958
RitaOak <oak.rita@hotmail.com>
2019-06-26 17:22:27 +0100

    chore: updated tabs to include main user pages

---
+++ src/components/groups.js
@@ -0,0 +1,22 @@
+import { html, css } from 'lit-element';
+import { PageViewElement } from './page-view-element.js';
+import { SharedStyles } from './shared-styles.js';
+
+class OAEGroups extends PageViewElement {
+  static get styles() {
+    return [
+      SharedStyles
+    ];
+  }
+
+  render() {
+    return html`
+      <section>
+        <h2>Groups go here</h2>
+        
+      </section>
+    `
+  }
+}
+window.customElements.define('user-groups', OAEGroups);
+
