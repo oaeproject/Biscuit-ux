5d56909002ba636e1c28635c24c3118334e7ffb9
Miguel Laginha <me@miguellaginha.com>
2019-07-04 16:25:13 +0100

    chore: added experimental support for bulma

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,27 +1,48 @@
 import {html, css} from 'lit-element';
 import {PageViewElement} from './page-view-element.js';
-// import {SharedStyles} from './shared-styles.js';
 
 import homepageStyles from './../../style/homepage.scss';
-import sharedStyles from './../../style/app.scss';
 import legacyStyles from '../../style/legacy.css';
 
 class OAEHomepage extends PageViewElement {
   static get styles() {
-    // return [SharedStyles];
-    return [homepageStyles, sharedStyles, legacyStyles];
+    return [homepageStyles /*, sharedStyles */, legacyStyles];
   }
 
   render() {
     return html`
       <style include="legacy"></style>
       <style include="homepage"></style>
-      <style include="app"></style>
-      <section>
-        <h2>This is the homepage</h2>
-        <button class="foo-button mdc-button">Button</button>
-      </section>
+      <h1 class="title">
+        Bulma
+      </h1>
+
+      <p class="subtitle">
+        Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+      </p>
+
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Input">
+        </div>
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div class="buttons">
+        <a class="button is-primary">Primary</a>
+        <a class="button is-link">Link</a>
+      </div>
     `;
   }
 }
+
 window.customElements.define('oae-landing-page', OAEHomepage);
