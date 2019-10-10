12808b0d5ae3f16d292148de434e6771c8d9a6e0
RitaOak <oak.rita@hotmail.com>
2019-10-10 12:55:00 +0100

    feat: test split screen

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,66 +1,24 @@
 import { html } from 'lit-element';
-import homepageStyles from '../../style/homepage.scss';
 import sharedStyles from '../../style/app.scss';
+import landingStyles from '../../style/landing-page.scss';
 import { PageViewElement } from './page-view-element.js';
 
 class OAEHomepage extends PageViewElement {
   static get styles() {
-    return [sharedStyles, homepageStyles];
+    return [sharedStyles, landingStyles];
   }
 
   render() {
     return html`
-      <section class="hero is-link is-fullheight">
-        <div class="hero-body">
-          <div class="container is-fluid is-fullheight is-mobile">
-            <div class="columns test">
-              <div class="column is-2">
-                <aside class="menu">
-                  <ul class="menu-list">
-                    <li>
-                      <a>
-                        <img class="sidebar" src="./../../node_modules/@mdi/svg/svg/view-dashboard-outline.svg" />
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <img class="sidebar" src="./../../node_modules/@mdi/svg/svg/view-dashboard-outline.svg" />
-                        Library</a
-                      >
-                    </li>
-                    <li>
-                      <a>
-                        <img class="sidebar" src="./../../node_modules/@mdi/svg/svg/view-dashboard-outline.svg" />
-                        Discussions</a
-                      >
-                    </li>
-                    <li>
-                      <a>
-                        <img class="sidebar" src="./../../node_modules/@mdi/svg/svg/view-dashboard-outline.svg" />
-                        User Settings</a
-                      >
-                    </li>
-                  </ul>
-                </aside>
-              </div>
-              <div class="column content-base">
-                <div class="column top-nav">
-                  <section>
-                    <button class="button is-primary is-rounded is-pulled-right">Username</button>
-                  </section>
-                  <section>
-                    <button class="button is-primary is-rounded is-pulled-right">Notifications</button>
-                  </section>
-                  <section>
-                    <button class="button is-primary is-rounded is-pulled-right">Context Change</button>
-                  </section>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="columns is-gapless">
+        <div class="column is-5">
+          dkfsdgkhsdkghkdsjhkgsjhdkfgjhfskdhgjkfdshjg
         </div>
-      </section>
+
+        <div class="column" style="overflow: auto;">
+          <h1>hfdhdshsogh</h1>
+        </div>
+      </div>
     `;
   }
 }
