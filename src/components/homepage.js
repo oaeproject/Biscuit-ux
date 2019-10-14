1b779a95c711f3d23903094565c3109f9a32d0c1
RitaOak <oak.rita@hotmail.com>
2019-10-14 02:57:56 +0100

    feat: split screen homepage (missing 100% div height)

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,4 +1,6 @@
 import { html } from 'lit-element';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icons/social-icons.js';
 import sharedStyles from '../../style/app.scss';
 import landingStyles from '../../style/landing-page.scss';
 import { PageViewElement } from './page-view-element.js';
@@ -10,13 +12,65 @@ class OAEHomepage extends PageViewElement {
 
   render() {
     return html`
-      <div class="columns is-gapless">
+      <nav class="navbar home-nav">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </a>
+          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </nav>
+
+      <div class="columns is-gapless is-flex">
         <div class="column is-5">
-          dkfsdgkhsdkghkdsjhkgsjhdkfgjhfskdhgjkfdshjg
+          <figure class="image home-img is-16by9">
+            <img
+              src="https://images.pexels.com/photos/356086/pexels-photo-356086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            />
+          </figure>
         </div>
 
-        <div class="column" style="overflow: auto;">
-          <h1>hfdhdshsogh</h1>
+        <div class="column home-rightCol">
+          <div class="home-right">
+            <h1 class="home-title">Search the OAE</h1>
+            <h6>
+              OAE is open source software created by universities for universities. It’s free and simple to get started.
+            </h6>
+            <div class="field home-search">
+              <div class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Search for folders, people or documents" />
+                <span class="icon is-left home-searchIcon">
+                  <iron-icon icon="search"></iron-icon>
+                </span>
+              </div>
+            </div>
+            <div class="dropdown is-hoverable home-drop">
+              <div class="dropdown-trigger">
+                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+                  <span>Filter by</span>
+                  <span class="icon is-small">
+                    <iron-icon icon="arrow-drop-down"></iron-icon>
+                  </span>
+                </button>
+              </div>
+              <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                <div class="dropdown-content">
+                  <div class="dropdown-item">
+                    <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     `;
