d3d1482bbe723b7563d1adf3fc894ef40752c3b8
RitaOak <oak.rita@hotmail.com>
2019-12-03 18:13:14 +0000

    style: sidebar size, links, colors

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,7 @@
 import { html, LitElement } from 'lit-element';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/social-icons.js';
+import '@polymer/iron-icons/av-icons.js';
 import homepageStyles from '../../style/homepage.scss';
 import sharedStyles from '../../style/app.scss';
 
@@ -19,34 +20,43 @@ class Sidebar extends LitElement {
         </div>
         <ul class="menu-list">
           <li>
+            <span class="icon sidebar-icon">
+              <iron-icon icon="dashboard"></iron-icon>
+            </span>
             <a class="dashboard">
               Dashboard
             </a>
           </li>
           <li>
+            <span class="icon sidebar-icon">
+              <iron-icon icon="av:library-books"></iron-icon>
+            </span>
             <a class="library">
               Library
             </a>
           </li>
           <li>
+            <span class="icon sidebar-icon">
+              <iron-icon icon="content-paste"></iron-icon>
+            </span>
             <a class="discussions">
               Discussions
             </a>
           </li>
           <li>
+            <span class="icon sidebar-icon">
+              <iron-icon icon="social:people-outline"></iron-icon>
+            </span>
             <a class="groups">
               Groups
             </a>
           </li>
           <li>
+            <span class="icon sidebar-icon">
+              <iron-icon icon="social:public"></iron-icon>
+            </span>
             <a class="network">
               Network
-            </a>
-          </li>
-          <hr class="navbar-divider" />
-          <li>
-            <a class="user-settings">
-              User Settings
             </a>
           </li>
         </ul>
