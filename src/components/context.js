5b60e5f1285c475fe2cafe7b2b00064ccd34f928
RitaOak <oak.rita@hotmail.com>
2019-12-05 13:02:21 +0000

    fix: indentation + removed unnecessary style files

--- src/components/context.js
+++ src/components/context.js
@@ -1,42 +1,41 @@
 import { html, LitElement } from 'lit-element';
-import homepageStyles from '../../style/homepage.scss';
 import sharedStyles from '../../style/app.scss';
+import TopnavButtonStyles from '../../style/topnav-buttons.scss';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/social-icons.js';
 import '@polymer/iron-icons/av-icons.js';
 import '@polymer/iron-icons/maps-icons.js';
-import contextStyles from '../../style/context.scss';
 
 class Context extends LitElement {
   static get styles() {
-    return [sharedStyles, homepageStyles, contextStyles];
+    return [sharedStyles, TopnavButtonStyles];
   }
 
   render() {
     return html`
-    <div class="navbar-item has-dropdown is-hoverable notifications-button">
-      <a class="navbar-link">
-      <span class="icon notification-icon">
-        <iron-icon icon="maps:map"></iron-icon>
-      </span>
-      <span class="notifications-label">Context</span>
-      </a>
-      <div class="navbar-dropdown">
-        <a class="navbar-item">
-          Notification 1
+      <div class="navbar-item has-dropdown is-hoverable notifications-button">
+        <a class="navbar-link">
+          <span class="icon notification-icon">
+            <iron-icon icon="maps:map"></iron-icon>
+          </span>
+          <span class="notifications-label">Context</span>
         </a>
-        <a class="navbar-item">
-          Notification 2
-        </a>
-        <a class="navbar-item">
-          Notification 3
-        </a>
-        <hr class="navbar-divider">
-        <div class="navbar-item">
-          See all
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Notification 1
+          </a>
+          <a class="navbar-item">
+            Notification 2
+          </a>
+          <a class="navbar-item">
+            Notification 3
+          </a>
+          <hr class="navbar-divider" />
+          <div class="navbar-item">
+            See all
+          </div>
         </div>
       </div>
-    </div>
     `;
   }
 }
--- src/components/notifications.js
+++ src/components/notifications.js
@@ -1,42 +1,41 @@
 import { html, LitElement } from 'lit-element';
-import homepageStyles from '../../style/homepage.scss';
 import sharedStyles from '../../style/app.scss';
+import TopnavButtonStyles from '../../style/topnav-buttons.scss';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/social-icons.js';
 import '@polymer/iron-icons/av-icons.js';
 import '@polymer/iron-icons/hardware-icons.js';
-import notificationStyles from '../../style/notifications.scss';
 
 class Notifications extends LitElement {
   static get styles() {
-    return [sharedStyles, homepageStyles, notificationStyles];
+    return [sharedStyles, TopnavButtonStyles];
   }
 
   render() {
     return html`
-    <div class="navbar-item has-dropdown is-hoverable notifications-button">
-      <a class="navbar-link">
-      <span class="icon notification-icon">
-        <iron-icon icon="social:notifications-none"></iron-icon>
-      </span>
-      <span class="notifications-label">Notifications</span>
-      </a>
-      <div class="navbar-dropdown">
-        <a class="navbar-item">
-          Notification 1
+      <div class="navbar-item has-dropdown is-hoverable notifications-button">
+        <a class="navbar-link">
+          <span class="icon notification-icon">
+            <iron-icon icon="social:notifications-none"></iron-icon>
+          </span>
+          <span class="notifications-label">Notifications</span>
         </a>
-        <a class="navbar-item">
-          Notification 2
-        </a>
-        <a class="navbar-item">
-          Notification 3
-        </a>
-        <hr class="navbar-divider">
-        <div class="navbar-item">
-          See all
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Notification 1
+          </a>
+          <a class="navbar-item">
+            Notification 2
+          </a>
+          <a class="navbar-item">
+            Notification 3
+          </a>
+          <hr class="navbar-divider" />
+          <div class="navbar-item">
+            See all
+          </div>
         </div>
       </div>
-    </div>
     `;
   }
 }
