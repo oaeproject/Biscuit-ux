db785ab7e353d3af6277c7cd3be39c6ec3f49ebc
RitaOak <oak.rita@hotmail.com>
2020-06-05 02:17:49 +0100

    fix: banner + avatar replacement

--- src/components/groups-avatar.js
+++ src/components/groups-avatar.js
@@ -10,7 +10,7 @@ class avatarGroups extends LitElement {
     return html`
       <section class="avatar">
         <figure class="image is-128x128 avatar-figure">
-          <img class="is-rounded" src="/images/landing-img.jpg" />
+          <img class="is-rounded" src="https://scontent.fopo1-1.fna.fbcdn.net/v/l/t1.0-9/537484_224892540989084_1267897031_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=XFXRaFbyn0AAX84b7iF&_nc_ht=scontent.fopo1-1.fna&oh=a63ebf898d71c55443aab13cb60ce95d&oe=5EFFF95A" />
         </figure>
       </section>
     `;
--- src/components/groups-banner.js
+++ src/components/groups-banner.js
@@ -10,7 +10,7 @@ class bannerGroups extends LitElement {
     return html`
       <section>
         <figure class="image is-1by3 is-fullwidth banner">
-          <img src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <img src="https://www.apereo.org/sites/default/files/styles/slider_large/public/OA2020%20Logo%20Flat%20w1200.jpg?itok=qTI6Np7z" />
         </figure>
       </section>
     `;
